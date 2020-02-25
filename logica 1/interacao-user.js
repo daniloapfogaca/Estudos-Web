ed9a01a1d23e0eabb1fdfb5e5da39519eb46ced4
Danilo Fogaça <daniloapfogaca@gmail.com>
2020-02-25 18:54:29 -0300

    estudos web, melhorando paginas com bootstrap 4

---
+++ logica 1/interacao-user.js
@@ -0,0 +1,22 @@
+<meta charset="UTF-8">
+<script>
+
+	function perguntaNome(){
+		document.write("Qual seu nome??");
+
+	}
+	function perguntaPeso(){
+		documente.write("Qual seu peso?");
+	
+	}
+	function perguntaAltura(){
+		document.wirte("Qual sua altura?")
+	}
+	
+	var nome = prompt(perguntaNome);
+	var peso = prompt(perguntaPeso);
+	var altura = prompt(perguntaAltura);
+	var = imc = peso / (altura * altura);
+	
+	alert(nome + ", seu IMC é" + imc);
+</script>
